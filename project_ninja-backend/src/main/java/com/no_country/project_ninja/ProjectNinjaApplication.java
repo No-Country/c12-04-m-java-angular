339ab75dc8c6f3d297a2d32cd46ff4fcbb40115d
Iran Mederos <iranmederos@gmail.com>
2023-07-05 14:38:52 -0300

    init api

---
+++ project_ninja-backend/src/main/java/com/no_country/project_ninja/ProjectNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.no_country.project_ninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNinjaApplication.class, args);
+	}
+
+}
