13ba568b9a341f82415d10b44202a991782642a8
Javier <javoalon@gmail.com>
2023-07-09 20:49:27 -0300

    Added (modal + funciones) de agregar Proyecto.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePageComponent } from './home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 interface Proyecto {
   name: string;
   description: string;
+  id: number;
+  spaces: { spaceName: string }[]
 }
 
 @Component({
@@ -12,6 +14,15 @@ interface Proyecto {
 })
 export class HomePageComponent implements OnInit {
   proyectos: Proyecto[] = [];
+  modalVisible: boolean = false;
+  projectEditing: Proyecto = {
+    name: '',
+    description: '',
+    id: 0,
+    spaces: [{
+      spaceName: '',
+    }]
+  };
 
   ngOnInit() {
     this.obtenerProyectos();
@@ -19,9 +30,49 @@ export class HomePageComponent implements OnInit {
 
   obtenerProyectos() { //Simulo el listado de proyecto que me devuelve
     this.proyectos = [
-      { name: 'Proyecto 1', description: 'Descripcion del proyecto 1'},
-      { name: 'Proyecto 2', description: 'Descripcion del proyecto 2' },
-      { name: 'Proyecto 3', description: 'Descripcion del proyecto 3' }
+      { name: 'Proyecto 1', description: 'Descripción del proyecto 1', id: 0, spaces:[{spaceName:'Espacio 1'}]}
     ];
+  }
+  openModal(proyecto: Proyecto) {
+    this.projectEditing = proyecto;
+    this.modalVisible = true;
+  }
+  closeModal() {
+    this.modalVisible = false;
+    this.modalNewProjectVisible = false;
+  }
+
+  //Funciones para el Modal de crear un nuevo proyecto
+  modalNewProjectVisible: boolean = false;
+  textoTituloEditable: string = 'Nuevo Proyecto';
+  textoDescripcionEditable: string = 'Descripcion de este proyecto';
+  textoNombreNuevoEspacio: string = 'Espacio';
+
+  openModalCreateProject() {
+    if (this.proyectos.length < 3) {
+      this.modalNewProjectVisible = true;
+    }
+  }
+
+  editandoTexto: boolean = false;
+  activarEdicion() {
+    this.editandoTexto = true;
+  }
+  guardarTexto() {
+    this.editandoTexto = false;
+  }
+  crearProyecto() {
+    const nuevoProyecto: Proyecto = {
+      name: this.textoTituloEditable,
+      description: this.textoDescripcionEditable,
+      id: this.proyectos.length,
+      spaces: [{spaceName: this.textoNombreNuevoEspacio}]
+    };
+    this.proyectos.push(nuevoProyecto);
+    this.editandoTexto = false;
+
+    this.textoTituloEditable = 'Nuevo Proyecto' //Reset de los valores para cuando se cree otro proyecto
+    this.textoDescripcionEditable = 'Descripcion de este proyecto';
+    this.closeModal();
   }
 }
