134f249f1e2d1587b429998418476cff6905675a
Agustin Rios <agrios@frba.utn.edu.ar>
2023-07-15 14:14:55 -0300

    modal confirm edit space

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectComponent } from './project/project.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ModalEditSpaceComponent } from './modal-edit-space/modal-edit-space.component';
+import { ModalConfirmDeleteSpaceComponent } from './modal-confirm-delete-space/modal-confirm-delete-space.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectComponent
+    ProjectComponent,
+    ModalEditSpaceComponent,
+    ModalConfirmDeleteSpaceComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/modal-confirm-delete-space/modal-confirm-delete-space.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirm-delete-space',
+  templateUrl: './modal-confirm-delete-space.component.html',
+  styleUrls: ['./modal-confirm-delete-space.component.scss']
+})
+export class ModalConfirmDeleteSpaceComponent {
+
+}
