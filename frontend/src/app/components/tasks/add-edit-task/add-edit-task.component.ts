a3c5465cda4b814c47fa06362b7362e6a7ed131e
esantose <easantose@gmail.com>
2023-07-26 16:26:25 -0400

    Actualizados cambios de Javi/Gary 26/Julio 04:25PM
    Modulo de tareas functions to add/edit/delete

--- frontend/src/app/components/tasks/add-edit-task/add-edit-task.component.ts
+++ frontend/src/app/components/tasks/add-edit-task/add-edit-task.component.ts
@@ -1,11 +1,19 @@
 import { Component, Inject } from '@angular/core';
-import { TaskModel} from './../../../models/task.model';
+import { IPriorities, TaskModel} from './../../../models/task.model';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { TaskService } from 'src/app/services/task.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActionTypes } from '@shared/action-types.enum';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';	
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DatePipe } from '@angular/common'
+
+
+interface DTOPutTask {
+	nameTask: string;
+  }
 
 @Component({
   selector: 'app-add-edit-task',
@@ -13,6 +21,9 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./add-edit-task.component.scss'],
 })
 export class AddEditTaskComponent {
+
+	apiUrl: string = "https://ninja-app-v1-api.azure-api.net/";	
+
 	actionTypes: ActionTypes | undefined;
 	id_Index: number;
 	isVisible = true;
@@ -22,7 +33,16 @@ export class AddEditTaskComponent {
   dataSelected: any = {
     spaceId: 0,
     task: null
-  }  
+  };
+  
+  newTask: DTOPutTask = {
+    nameTask: `task`
+  }
+  
+  priority: IPriorities = {
+    id: 1,
+    namePriority: 'Bajo',
+  };
 
   users = [
     {name: 'Percy'},
@@ -35,34 +55,15 @@ export class AddEditTaskComponent {
 
   actionTitle: string = 'Agregar Tarea';
 
-//   constructor(
-// 		private fb: FormBuilder,
-// 		private taskService: TaskService,
-// 		private router: Router,
-// 		private aRoute: ActivatedRoute,
-// 		public snackBar: MatSnackBar,
-// 		public dialogRef: MatDialogRef<AddEditTaskComponent>
-// 	) {
-// 		this.myForm = this.fb.group({
-// 			// name: ['', [Validators.required, Validators.maxLength(15)]],
-// 			// description: ['', [Validators.required]],
-// 			name: [''],
-// 			description: [''],			
-// 			dueDate: [Date.now],
-// 			status: [''],
-// 		});
-
-// 		const idParam = 'id';
-// 		this.id_Index = this.aRoute.snapshot.params[idParam];
-// 	}
-
 constructor(
+		private httpClient: HttpClient,
 		private fb: FormBuilder,
 		private taskService: TaskService,
 		private router: Router,
 		private aRoute: ActivatedRoute,
 		public snackBar: MatSnackBar,	
 		private dialogRef: MatDialogRef<AddEditTaskComponent>,
+		public datepipe: DatePipe,
 		@Inject(MAT_DIALOG_DATA) public data: any
   ){
 	console.log('data..', data)
@@ -77,15 +78,6 @@ constructor(
 		
     });
 
-	// this.myForm.patchValue({
-	// 	nameTask: data.nameTask,
-	// 	description: data.description,
-	// 	dueDate: data.dueDate,
-	// 	priorityTask: data.priorityTask,
-	// 	status: data.status,
-	// });
-
-
 	this.actionTypes = data.task.id === 0 ? ActionTypes.ADD : ActionTypes.EDIT;
 	this.actionTitle = data.task.id === 0 ? 'Agregar Tarea' : 'Editar Tarea';
 
@@ -96,48 +88,85 @@ constructor(
 
   }
 
-	ngOnInit(): void {
-		// this.actionTypes = this.id_Index === undefined ? ActionTypes.ADD : ActionTypes.EDIT;
-		// this.actionTitle = this.id_Index === undefined ? 'Agregar Tarea' : 'Editar Tarea';
+ngOnInit(): void {
 		this.isVisible = this.actionTypes === ActionTypes.EDIT;
 	}
 
-	onCancelDialog(): void {
+
+onCancelDialog(): void {
 		this.dialogRef.close();
 	  }
 
+	  priorityGroup = 0;
+	  priorityValue = 0;
+	  onPriorityChange() {
+		this.priorityValue= this.priorityGroup;
+		console.log('priorityValue', this.priorityValue)
+	  }	
+	  
+	  statusGroup = false;
+	  statusValue = false;
+	  onStatusChange() {
+		this.statusValue= this.statusGroup;
+		console.log('statusValue', this.statusValue)
+	  }		  
 
-  SaveTask() {
-	const task: TaskModel = {
+SaveTask() {
+	const task: any = {
 	  id: this.actionTypes === ActionTypes.ADD ? 0 : this.myForm.value.id,
       nameTask: this.myForm.value.nameTask,
       description: this.myForm.value.description,
-	  dueDate: this.myForm.value.dueDate, //new Date('2022-10-31'),
-	  priorityTask: this.myForm.value.priorityTask,
-	  status: this.actionTypes === ActionTypes.ADD ? '' : this.myForm.value.status
+	  dueDate: this.datepipe.transform(this.myForm.value.dueDate, 'dd/MM/yyyy'),
+	  priorityTask: this.priorityValue, //this.priority, // this.myForm.value.priorityTask,
+	  status: this.statusValue //this.actionTypes === ActionTypes.ADD ? false : true //this.myForm.value.status
     };
 	console.log('SaveTask', this.actionTypes);
+	console.log('this.myForm.value.priorityTask', this.myForm.value.priorityTask);
 
-		switch (this.actionTypes) {
-			case ActionTypes.ADD:
-				this.addTask(task);
-				break;
-			case ActionTypes.EDIT:
-				this.editTask(task);
-				break;
+
+	switch (this.actionTypes) {
+		case ActionTypes.ADD:
+				// this.addTask(task);
+				this.createTask();
+			break;
+		case ActionTypes.EDIT:
+			this.editTask(task);
+		break;
+	}
+	this.dialogRef.close();
+}
+
+createTask(){
+		this.newTask.nameTask = this.myForm.value.nameTask;
+ 
+        this.addTaskNew(this.newTask).subscribe({
+			error: error => {
+			  console.error('Error al crear el objeto****', error);
+			  if (error.status===200) {
+				console.log('La tarea ha sido creada con exito.');
+				this.snackBar.open('La tarea ha sido creada con exito!!!', 'Completado', {
+					duration: 4000, verticalPosition: "top", horizontalPosition: "end" 
+				  }); 
+			  }
+			},
+			complete: () => {
+			  console.log('La tarea ha sido creada con exito.');
+			  this.snackBar.open('La tarea ha sido creada con exito!!!', 'Completado', {
+				duration: 4000, verticalPosition: "top", horizontalPosition: "end" 
+			  }); 
+			},
+		  });		
 		}
+		 
+
+addTaskNew(dTOAddTask:any): Observable<any>{
+		let url = this.apiUrl + `task?spaceId=${this.dataSelected.spaceId}`;
+		console.log('createTask-addTask', dTOAddTask);
+		console.log('createTask-url', url);
+		return this.httpClient.post<any>(url, dTOAddTask);
 	}
 
-	// createTask(){
-	// 	this.addTask(this.newSpace).subscribe((data) => {
-	// 	   console.log('Objeto creado correctamente');
-	// 	   window.location.reload();
-	// 	 },
-	// 	 (error) => {
-	// 	   console.error('Error al crear el objeto', error);
-	// 	 })}
-
-	addTask(task: TaskModel) {
+addTask(task: TaskModel) {
 		console.log('add-edit/addTask', task.nameTask);
 		console.log('add-edit/spaceId', this.dataSelected.spaceId);
 		// this.taskService.addTask(task.nameTask,  this.dataSelected.spaceId);
@@ -150,18 +179,40 @@ constructor(
 			// 	console.error('Error al crear el objeto', error);
 			// })};
 			
-		this.snackBar.open('Tarea agregada!', '', { duration: 3000 });
-		// this.router.navigate(['home']);
+			this.snackBar.open('La tarea ha sido creada con exito!!!', 'Completado', {
+				duration: 4000, verticalPosition: "top", horizontalPosition: "end" 
+			  }); 
 	}
 
-	editTask(task: TaskModel) {
+editTask(task: any) {
 		console.log('add-edit/editTask', task);
-		// this.taskService.editTask(task, this.id_Index);
-		// this.snackBar.open('The task has been updated succesfuly!', '', {
-		// 	duration: 3000,
-		// });
+		this.editTaskNew(task.id, task).subscribe({
 
-		this.router.navigate(['tasks']);
-	}  
+			error: error => {
+				console.error('Error al editar el objeto****', error);
+				if (error.status===200) {
+				  console.log('La tarea ha sido editada con exito.');
+				  this.snackBar.open('La tarea ha sido editada con exito!!!', 'Completado', {
+					duration: 4000, verticalPosition: "top", horizontalPosition: "end" 
+				  });
+				}
+			  },
+			  complete: () => {
+				console.log('La tarea ha sido editada con exito.');
+				this.snackBar.open('La tarea ha sido editada con exito!!!', 'Completado', {
+				  duration: 4000, verticalPosition: "top", horizontalPosition: "end" 
+				}); 
+			  },			  			
+
+		})
+	}
+	
+	editTaskNew(id: number, dTOPutSpace:TaskModel): Observable<any>{
+		let url = this.apiUrl + `task?id=${id}`;		
+		console.log('editTaskNew-addTask', dTOPutSpace);
+		console.log('editTaskNew-url', url);
+
+		return this.httpClient.put(url, dTOPutSpace)
+	  }
 
 }
---
+++ frontend/src/app/shared/show-notification/show-notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-notification',
+  templateUrl: './show-notification.component.html',
+  styleUrls: ['./show-notification.component.scss']
+})
+export class ShowNotificationComponent {
+
+}
