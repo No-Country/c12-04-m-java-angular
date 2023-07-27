32b1437e652a24742efb29d96561aabaa7a89619
Agustin Rios <agustin528rios@gmail.com>
2023-07-27 16:36:11 -0300

    arreglo de mayúsculas

--- frontend/src/app/components/tasks/list-tasks/list-tasks.component.ts
+++ frontend/src/app/components/tasks/list-tasks/list-tasks.component.ts
@@ -24,7 +24,7 @@ export class ListTasksComponent implements OnInit {
   @ViewChild(MatSort, { static: true }) sort!: MatSort;
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   
-  displayedColumns: string[] = ['status', 'nameTask', 'description', 'dueDate', 'priorityTask','acciones'];
+  displayedColumns: string[] = ['status', 'description', 'dueDate', 'priorityTask','acciones'];
   priority: IPriorities = {
     id: 1,
     namePriority: '',
